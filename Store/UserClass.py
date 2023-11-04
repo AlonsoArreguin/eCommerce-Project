781e3e591e3ad354472cab87d2b8381e490d9a59
Alonso Arreguin <alonsoarreguin2001@gmail.com>
2023-11-04 09:19:52 -0500

    Updated UserClass.py

--- Store/UserClass.py
+++ Store/UserClass.py
@@ -1,3 +1,5 @@
+import sqlite3
+
 class User:
     def __init__(self, databaseName="", tableName="", loggedIn=False, userID=""):
         self.databaseName = databaseName
@@ -6,23 +8,56 @@ class User:
         self.userID = userID
 
     def login(self):
+        
+        if not self.loggedIn:
+            username = input("Enter your username: ")
+            password = input("Enter your password: ")
 
-        pass
+            if """add authenticatio method here"""
+                self.loggedIn = True
+                self.userID = username
+                print("Login successful. Welcome, {}!".format(username))
+                return True
+            else:
+                print("Login failed, Invalid username or password.")
+                return False
+        else:
+            print("User already logged in.")
+            return False
 
     def logout(self):
 
-        pass
+        if self.loggedIn:
+            print("Logout successful. Goodbye, {}!".format(self.userID))
+            self.loggedIn = False
+            self.userID = ""
+            return True
+        else:
+            print("User not logged in.")
+            return False
 
     def viewAccountInformation(self):
 
-        pass
+        if self.loggedIn:
+            print("Account information for userID: ", self.userID)
+        else:
+            print("User not logged in. Please login to view account inforation.")
 
     def createAccount(self):
 
-        pass
+        if not self.loggedIn:
+            new_username = input("Enter username for new account: ")
+            new_password = input("Enter password for new account: ")
+
+            """add authentication method here"""
+            print("Account for {} created successfully!".format(new_username))
+        else:
+            print("username already in used. Cannot create a new account.")
 
     def getLoggedIn(self):
+
         return self.loggedIn
 
     def getUserID(self):
+
         return self.userID
