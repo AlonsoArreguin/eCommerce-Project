05d8a1d4491fbd6d3522926f330b0fa799f19450
alieftwu <alieftwu@gmail.com>
2023-11-05 09:03:29 -0600

    added searchInventory function

--- Store/InventoryClass.py
+++ Store/InventoryClass.py
@@ -37,10 +37,24 @@ class Inventory:
                 print(f"ISBN: {row[0]}, Title: {row[1]}, Author: {row[2]}, Genre: {row[3]}, Pages: {row[4]}, Release Date: {row[5]}, Stock: {row[6]}")
         else:
             print("Inventory is empty.")
+    def searchInventory(self):
+        title =  input("Enter title to search: ")
+        cursor = connection.cursor()
+        cursor.execute(f"SELECT * FROM inventory WHERE Title LIKE ?", (f"%{title}%",))
+        rows = cursor.fetchall()
+        if rows:
+            #Displays specific title user inputted
+            for row in rows:
+                print(f"ISBN: {row[0]}, Title: {row[1]}, Author: {row[2]}, Genre: {row[3]}, Pages: {row[4]}, Release Date: {row[5]}, Stock: {row[6]}")
+        else:
+            print(f"The title '{title}' was not found in the inventory.")
         
-#Test variables    
+#Test variables for view inventory   
 #inventory =  Inventory(databaseName = "inventorydatabase.db")
 #inventory.viewInventory(databaseName = "inventorydatabase.db")
+#Test variables for search inventory
+#inventory = Inventory(databaseName = "inventorydatabase.db")
+#inventory.searchInventory()
 cursor.close()
 connection.close()
     
