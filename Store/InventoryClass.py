a86b913cafb92841a00f4e8fee44f9db36e6d05b
alieftwu <alieftwu@gmail.com>
2023-11-05 09:53:49 -0600

    Added decreaseStock and getters & setters

--- Store/InventoryClass.py
+++ Store/InventoryClass.py
@@ -25,7 +25,15 @@ class Inventory:
         self.databaseName = databaseName
         self.tableName = tableName
         self.isbn = isbn
-        
+        #setters and getters
+    def getDatabaseName(self):
+        return self.databaseName
+    def setDatabaseName(self, databaseName):
+        self.databaseName = databaseName
+    def getTableName(self):
+        return self.tableName
+    def setTableName(self, tableName):
+        self.tableName = tableName
     def viewInventory(self, databaseName):
         self.databaseName = databaseName
         cursor = connection.cursor()
@@ -48,13 +56,26 @@ class Inventory:
                 print(f"ISBN: {row[0]}, Title: {row[1]}, Author: {row[2]}, Genre: {row[3]}, Pages: {row[4]}, Release Date: {row[5]}, Stock: {row[6]}")
         else:
             print(f"The title '{title}' was not found in the inventory.")
-        
+    def decreaseStock(self, isbn):
+        self.isbn = isbn
+        isbn = input("Enter ISBN to search: ")
+        cursor = connection.cursor()
+        cursor.execute(f"UPDATE Inventory SET Stock = Stock - 1 WHERE isbn = ?", (isbn, ))
+        connection.commit()
+        if cursor.rowcount > 0:
+            print(f"ISBN {isbn} stock decreased successfully.")
+        else:
+            print(f"{isbn} not found in the inventory. Failed to decrease the stock.")
+    
 #Test variables for view inventory   
 #inventory =  Inventory(databaseName = "inventorydatabase.db")
 #inventory.viewInventory(databaseName = "inventorydatabase.db")
 #Test variables for search inventory
 #inventory = Inventory(databaseName = "inventorydatabase.db")
 #inventory.searchInventory()
+#Test variables for decreaseStock
+#inventory = Inventory(databaseName = "inventorydatabase.db")
+#inventory.decreaseStock("isbn")
 cursor.close()
 connection.close()
     
