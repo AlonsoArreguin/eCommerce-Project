32c9b29448cfebe78acee7a1e7846586fe6eeb53
alieftwu <alieftwu@gmail.com>
2023-11-15 17:04:49 -0600

    started main menu skeleton

---
+++ Store/main.py
@@ -0,0 +1,15 @@
+from InventoryClass import Inventory, connection, cursor
+
+
+#create database tables if they don't exist here
+#create sample dataset here if they do not exit
+
+def startMenu():
+
+def accountSettings():
+
+def mainMenu():
+
+def inventoryMenu():
+
+def cartMenu():
